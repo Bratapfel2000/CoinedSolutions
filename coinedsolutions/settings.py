b2c16599c2d969e31198241b98eb4aafa2e6b683
Bratapfel <bratapfel2k@gmail.com>
2021-01-27 10:53:32 +0100

    updated settings

--- coinedsolutions/settings.py
+++ coinedsolutions/settings.py
@@ -22,13 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '+(_u*os4yb=3-9uqmgsp54(y($g(5&4ppl-axal5y8zb4v5wgz'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['coinedsolutions.herokuapp.com']
 
 
 # Application definition
