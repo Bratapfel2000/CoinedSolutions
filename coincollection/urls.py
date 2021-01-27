2fa1af9b7ff2eb81eee3aec24832b24fc55398c9
Bratapfel <bratapfel2k@gmail.com>
2021-01-27 14:48:59 +0100

    add country.html in coins/templates and to urls / views

--- coincollection/urls.py
+++ coincollection/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     #path('coinhome/', views.coinhome, name='blog-home'),
     path('', views.coinhome, name='coin-home'),
+    path('country/<int:id>/<slug:slug>', views.country, name='country'),
 
 ]
--- coincollection/views.py
+++ coincollection/views.py
@@ -16,3 +16,13 @@ def coinhome(request):
 	order = myFilter.qs
 	context={'category':category, 'all_products':all_products, 'order':order, 'myFilter':myFilter}
 	return render(request, 'coincollection/home.html',context)
+
+def country(request,id,slug):
+    products = Product.objects.filter(category_id=id) 
+    catdata = Category.objects.get(pk=id)
+    category = Category.objects.all()
+    context={'products': products,
+             'category':category,
+             'catdata':catdata,
+             'slug': slug}
+    return render(request,'coincollection/country.html',context)
