80d8356f6ac61bc7a612fbe891931d7cf3bffac9
Bratapfel <bratapfel2k@gmail.com>
2021-02-07 15:29:57 +0100

    Changes to be committed: 	modified:   blog/static/blog/main2.css
    	edited header blue to blue of eu flag (003399)
    
    	modified:   blog/templates/blog/base.html
    	actually nothing chaned much here
    
    new file:   coincollection/templates/coincollection/base.html
    added base to coincollection and added view for coin by country on sidebar
    
    	modified:   coincollection/templates/coincollection/coin_detail.html
    design change
    
    	new file:   coincollection/templates/coincollection/coinfilter.html
    added new page for filtering
    
    	modified:   coincollection/templates/coincollection/country.html
    design change
    
    	modified:   coincollection/templates/coincollection/home.html
    kicked out stuff and now only coins showing, rest on sidebar
    
    	new file:   coincollection/templates/coincollection/testing.html
    	modified:   coincollection/urls.py
    added coinfilter
    
    	modified:   coincollection/views.py
    added view for coincollection/base

--- coincollection/urls.py
+++ coincollection/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     #path('coinhome/', views.coinhome, name='blog-home'),
     path('', views.coinhome, name='coin-home'),
+    path('coinfilter/', views.coin_filter, name='coin-filter'),
     path('country/<int:id>/<slug:slug>', views.country, name='country'),
     path('category/<slug:slug>/<int:id>', views.coin_detail, name='coin-detail'),
 
--- coincollection/views.py
+++ coincollection/views.py
@@ -8,20 +8,33 @@ from .filters import ProductFilter
 
 
 def coinhome(request):
-	category = Category.objects.all()
-	all_products = Product.objects.all()
-	page = "somu-overview"
-	order = Product.objects.all()
-	myFilter = ProductFilter(request.GET, queryset=order)
-	order = myFilter.qs
-	context={'category':category, 'all_products':all_products, 'order':order, 'myFilter':myFilter}
-	return render(request, 'coincollection/home.html',context)
+    category = Category.objects.all()
+    all_products = Product.objects.all()
+    page = "somu-overview"
+    order = Product.objects.all()
+    myFilter = ProductFilter(request.GET, queryset=order)
+    order = myFilter.qs
+    context={'category':category, 'all_products':all_products, 'order':order, 'myFilter':myFilter}
+    return render(request, 'coincollection/home.html',context)
+
+
+def coinbase(request):
+    category = Category.objects.all()
+    all_products = Product.objects.all()
+    page = "somu-overview"
+    order = Product.objects.all()
+    myFilter = ProductFilter(request.GET, queryset=order)
+    order = myFilter.qs
+    context={'category':category, 'all_products':all_products, 'order':order, 'myFilter':myFilter}
+    return render(request, 'coincollection/base.html',context)
 
 def country(request,id,slug):
     products = Product.objects.filter(category_id=id) 
+    all_products = Product.objects.all()
     catdata = Category.objects.get(pk=id)
     category = Category.objects.all()
     context={'products': products,
+             'all_products':all_products, 
              'category':category,
              'catdata':catdata,
              'slug': slug}
@@ -33,3 +46,13 @@ def coin_detail(request,id,slug):
     context = {'product': product,'category': category,
                }
     return render(request,'coincollection/coin_detail.html',context)
+
+def coin_filter(request):
+    category = Category.objects.all()
+    all_products = Product.objects.all()
+    page = "somu-overview"
+    order = Product.objects.all()
+    myFilter = ProductFilter(request.GET, queryset=order)
+    order = myFilter.qs
+    context={'category':category, 'all_products':all_products, 'order':order, 'myFilter':myFilter}
+    return render(request,'coincollection/coinfilter.html',context)
