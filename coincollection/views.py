b48429d9b686379551810689b9e5c8a4da6c40e9
Bratapfel <bratapfel2k@gmail.com>
2021-02-22 15:31:22 +0100

    added unification_histogram in views to unify substates of countries and added all in html charts

--- coincollection/views.py
+++ coincollection/views.py
@@ -83,18 +83,45 @@ def histogram(s):
             d[c] += 1
     return d
 
+def unification_histogram(s):
+    d = dict()
+    for c in s:
+        if c.parent == None:
+            if c not in d:
+                d[c] = 1
+            else:
+                d[c] += 1
+        else:
+            if c.parent not in d:
+                d[c.parent] = 1
+            else:
+                d[c.parent] += 1
+    return d
+
 def coin_charts(request):
-    labels = []
-    data = []
     all_products = Product.objects.all()
     category = Category.objects.all()
+    labels = []
+    data = []
     product_listed = []
+    #adds hist with number of countries in list for charts
     for prod in all_products:
         product_listed.append(prod.category)
     product_hist = histogram(product_listed)
     for i in product_hist:
-        #print(i, product_hist[i])
         data.append(product_hist[i])
         labels.append(i.title)
-    context={'category':category, 'all_products':all_products,'labels': labels, 'data': data}
+
+    #unified subcountries
+    product_listed_unified = []
+    labels_unified = []
+    data_unified = []
+    for prod in all_products:
+        product_listed_unified.append(prod.category)
+    product_hist_unfied = unification_histogram(product_listed_unified)
+    for j in product_hist_unfied:
+        data_unified.append(product_hist_unfied[j])
+        labels_unified.append(j.title)
+
+    context={'category':category, 'all_products':all_products,'labels': labels, 'data': data, 'labels_unified':labels_unified, 'data_unified':data_unified}
     return render(request, 'coincollection/coin_charts.html', context)
