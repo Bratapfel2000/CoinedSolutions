1e3c322727332b8dc2e11ffb6b5a45779eb77ccd
patrickhaetti <patrickhaetti@gmx.de>
2022-02-09 18:09:04 +0100

    take out stuff from serializer

--- api/serializers.py
+++ api/serializers.py
@@ -5,5 +5,6 @@ from coincollection.models import Product
 class CoinProductSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Product
-		fields = ('LETTERS', 'coin_choice_field', 'coin_sort_field', 'title', 'description', 'year', 'date_found', 'category')
+		##fields = ('LETTERS', 'coin_choice_field', 'coin_sort_field', 'title', 'description', 'year', 'date_found', 'category')
+		fields = ('LETTERS')
 
