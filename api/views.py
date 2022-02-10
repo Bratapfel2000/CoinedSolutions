d993b36e1d9b6122ac1ed5594c216ff7ec25e63f
patrickhaetti <patrickhaetti@gmx.de>
2022-02-10 10:56:18 +0100

    add permissions.py, change views and settings to isauthorreadonly

--- api/views.py
+++ api/views.py
@@ -3,14 +3,14 @@ from blog.models import Post
 from .serializers import PostSerializer
 from coincollection.models import Product
 from .serializers import CoinProductSerializer
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class PostAPIView(generics.ListAPIView):
 	queryset = Post.objects.all()
 	serializer_class = PostSerializer
-	permission_classes = (AllowAny,)
+	permission_classes = (IsAuthenticatedOrReadOnly,)
 
 class CoinProductAPIView(generics.ListAPIView):
 	queryset = Product.objects.all()
 	serializer_class = CoinProductSerializer
-	permission_classes = (AllowAny,)
+	permission_classes = (IsAuthenticatedOrReadOnly,)
