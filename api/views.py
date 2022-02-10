2813f871ec9a9afb77686978d50fb24853e681af
patrickhaetti <patrickhaetti@gmx.de>
2022-02-10 10:52:46 +0100

    allowany in views

--- api/views.py
+++ api/views.py
@@ -3,14 +3,14 @@ from blog.models import Post
 from .serializers import PostSerializer
 from coincollection.models import Product
 from .serializers import CoinProductSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import AllowAny
 
 class PostAPIView(generics.ListAPIView):
 	queryset = Post.objects.all()
 	serializer_class = PostSerializer
-	permission_classes = (IsAuthenticatedOrReadOnly,)
+	permission_classes = (AllowAny,)
 
 class CoinProductAPIView(generics.ListAPIView):
 	queryset = Product.objects.all()
 	serializer_class = CoinProductSerializer
-	permission_classes = (IsAuthenticatedOrReadOnly,)
+	permission_classes = (AllowAny,)
