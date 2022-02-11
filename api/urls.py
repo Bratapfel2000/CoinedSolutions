87dbf5b620351a5cb429eff8aa9ce45fac7520c2
patrickhaetti <patrickhaetti@gmx.de>
2022-02-11 14:12:40 +0100

    disable api urls

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+"""from django.urls import path
 from .views import PostAPIView
 from .views import CoinProductAPIView
 
 urlpatterns = [
 	path('blogs/', PostAPIView.as_view()),
 	path('coins/', CoinProductAPIView.as_view()),
-]
+]"""
